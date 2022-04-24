6e72e5f29170e6a8fcd8f61232290f8e75f3b1c5
mabbuprem <mabbupremkumar@gmail.com>
2022-04-24 09:26:53 +0530

    [prem] FEATURE ADDED:Adding Employee Data from Database with ADO.NET

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Payroll_ADO.NET
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll Database");
+        }
+    }
+}
+
